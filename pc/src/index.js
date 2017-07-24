fa81cd51f806b1431dc583c76482c3465565769f
wangxiaoqing <xiaoqingwang@126.com>
2017-07-24 22:34:26 +0800

    Warning: Unknown prop `onTouchTap` on <button> tag. Remove this prop from the element.     in button (created by EnhancedButton)

--- pc/src/index.js
+++ pc/src/index.js
@@ -4,12 +4,15 @@ import Root from './env/root';
 import store,{sagaMiddleware} from './env/store';
 import rootSaga from './sagas';
 import registerServiceWorker from './registerServiceWorker';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import {
     postNotifyFromJPush
 } from './env/jpush';
 import {
   registerandroid
 } from './env/android';
+
+injectTapEventPlugin();
 
 ReactDOM.render(
     <Root />,
