b893e55fa5863688626d7082b7f605011aa3a5d5
wangxiaoqing <xiaoqingwang@126.com>
2017-07-24 10:55:06 +0800

    msg

--- test/kafkaconsumergroup.js
+++ test/kafkaconsumergroup.js
@@ -24,6 +24,7 @@ function onError (error) {
 
 function onMessage (message) {
   console.log('(%s)%s read msg Topic="%s" Partition=%s Offset=%d',cid, this.client.clientId, message.topic, message.partition, message.offset);
+  console.log(`获取到消息:${JSON.stringify(message)}`);
 }
 
 process.once('SIGINT', ()=> {
