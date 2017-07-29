20fd32b26d1ee652d7d1c0e83acc56f3377d3ed6
DESKTOP-6EU71M3\杨维克 <yangweike@hotmail.com>
2017-07-29 21:15:55 +0800

    no message

--- admin/src/App.js
+++ admin/src/App.js
@@ -46,17 +46,17 @@ class App extends Component {
             >
             <Resource name="systemconfig" list={SystemconfigList} show={SystemconfigShow} edit={SystemconfigEdit} create={SystemconfigCreate} />
             <Resource name="canrowdata" list={CanRawDataList} show={CanRawDataShow} />
-              <Resource name="devicegroup" list={DeviceGroupList} show={DeviceGroupShow} edit={DeviceGroupEdit} create={DeviceGroupCreate} />
-              <Resource name="device" list={DeviceList} show={DeviceShow} edit={DeviceEdit} create={DeviceCreate} />
-              <Resource name="historytrack" list={HistoryTrackList} show={HistoryTrackShow} />
-              <Resource name="realtimealarm" list={RealtimeAlarmList} show={RealtimeAlarmShow} />
-              <Resource name="user" list={UserList} show={UserShow} edit={UserEdit} create={UserCreate} />
-              <Resource name="userlog" list={UserlogList} />
-              <Resource name="permission" list={PermissionList} edit={PermissionEdit} create={PermissionCreate} />
-              <Resource name="useradmin" list={UserAdminList} edit={UserAdminEdit} create={UserAdminCreate} />
-              <Resource name="usergroup" list={UserGroupList} show={UserGroupShow} edit={UserGroupEdit} create={UserGroupCreate} />
-
-
+            <Resource name="devicegroup" list={DeviceGroupList} show={DeviceGroupShow} edit={DeviceGroupEdit} create={DeviceGroupCreate} />
+            <Resource name="device" list={DeviceList} show={DeviceShow} edit={DeviceEdit} create={DeviceCreate} />
+            <Resource name="historytrack" list={HistoryTrackList} show={HistoryTrackShow} />
+            <Resource name="realtimealarm" list={RealtimeAlarmList} show={RealtimeAlarmShow} />
+            <Resource name="user" list={UserList} show={UserShow} edit={UserEdit} create={UserCreate} />
+            <Resource name="usergroup" list={UserGroupList} show={UserGroupShow} edit={UserGroupEdit} create={UserGroupCreate} />
+            <Resource name="userlog" list={UserlogList} />
+            <Resource name="permission" list={PermissionList} edit={PermissionEdit} create={PermissionCreate} />
+            <Resource name="useradmin" list={UserAdminList} edit={UserAdminEdit} create={UserAdminCreate} />
+            <Resource name="usergroup" list={UserGroupList} show={UserGroupShow} edit={UserGroupEdit} create={UserGroupCreate} />
+            
             </Admin>
         );
     }
--- admin/src/LeftmenuData.js
+++ admin/src/LeftmenuData.js
@@ -6,11 +6,12 @@ import SystemconfigIcon from 'material-ui/svg-icons/action/settings-brightness';
 import DeviceIcon from 'material-ui/svg-icons/device/battery-charging-full';//设备
 import DeviceGroupIcon from 'material-ui/svg-icons/device/devices';//设备分组
 import UserIcon from 'material-ui/svg-icons/action/account-circle';//用户
+import UserGroupIcon from 'material-ui/svg-icons/action/supervisor-account';//用户分组
 import UserLogIcon from 'material-ui/svg-icons/action/book';//用户登陆日志
 import PermissionIcon from 'material-ui/svg-icons/action/info';//权限
 import RealtimeAlarmIcon from 'material-ui/svg-icons/action/alarm';//实时报警
 import HistoryTrackIcon from 'material-ui/svg-icons/action/history';//历史轨迹
-import CanrowDataIcon from 'material-ui/svg-icons/action/timeline';//历史轨迹
+import CanrowDataIcon from 'material-ui/svg-icons/action/timeline';//原始数据
 
 
 
@@ -19,6 +20,7 @@ export default [
         { name: 'device', icon: <DeviceIcon /> },
         { name: 'devicegroup', icon: <DeviceGroupIcon /> },
         { name: 'user', icon: <UserIcon /> },
+        { name: 'usergroup', icon: <UserGroupIcon /> },
         { name: 'permission', icon: <PermissionIcon /> },
         { name: 'realtimealaram', icon: <RealtimeAlarmIcon /> },
         { name: 'historytrack', icon: <HistoryTrackIcon /> },
