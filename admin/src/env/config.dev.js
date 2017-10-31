ab68865f664d31fe98f3a0755b690bfceda81660
wangxiaoqing <xiaoqingwang@126.com>
2017-10-31 17:38:02 +0800

    for test

--- admin/src/env/config.dev.js
+++ admin/src/env/config.dev.js
@@ -1,5 +1,5 @@
 let islocalhost = true;
-let serverurl = islocalhost?'http://localhost:5011':'http://bms.com28.cn';
+let serverurl = islocalhost?'http://localhost:5011':'http://bms.com28.cn/apisrv';
 const organizationid = '599af5dc5f943819f10509e6';
 export default {
     restserverurl:`${serverurl}/adminapi/v1/${organizationid}`,
