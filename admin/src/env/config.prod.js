47ad6601afeb71fc735cf92d0553efc8eafd3960
wangxiaoqing <xiaoqingwang@126.com>
2017-10-31 16:24:56 +0800

    admin

--- admin/src/env/config.prod.js
+++ admin/src/env/config.prod.js
@@ -1,4 +1,4 @@
-let islocalhost = true;
+let islocalhost = false;
 let serverurl = islocalhost?'http://localhost:5011':'http://bms.com28.cn';
 const organizationid = '599af5dc5f943819f10509e6';
 export default {
