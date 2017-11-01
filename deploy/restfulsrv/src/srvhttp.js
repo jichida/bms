51d4721bf68428cb90d1535b7d17f18faf4cced4
wangxiaoqing <xiaoqingwang@126.com>
2017-11-01 09:29:40 +0800

    处理options，允许跨域

--- deploy/restfulsrv/src/srvhttp.js
+++ deploy/restfulsrv/src/srvhttp.js
@@ -43,6 +43,13 @@ let startsrv = ()=>{
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  app.options("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  });
+
   app.use((req, res, next)=> {
       console.log('req.url:' + req.url);
       res.setHeader('Access-Control-Allow-Origin', '*');
