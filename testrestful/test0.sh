cd37d1e5c2ff4f4655029f547b27e6130b1ddf88
wangxiaoqing <xiaoqingwang@126.com>
2017-11-17 18:45:27 +0800

    添加依赖项

---
+++ testrestful/test0.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json" -X POST  --data @json0.json http://bms.com28.cn/apisrv/m2mgw/setdata
---
+++ testrestful/test1.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json" -X POST  --data @json1.json http://bms.com28.cn/apisrv/m2mgw/setdata
