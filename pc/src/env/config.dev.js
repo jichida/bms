5cabb15a18adf331dba424f6b671ebcfd6af05df
wangxiaoqing <xiaoqingwang@126.com>
2017-11-23 13:16:43 +0800

    ==部署到118.31.41.232==

--- pc/src/env/config.dev.js
+++ pc/src/env/config.dev.js
@@ -1,5 +1,5 @@
-let islocalhost = false;
-const serverurl = islocalhost?'http://localhost:5011':'http://bms.com28.cn';
+let islocalhost = true;
+const serverurl = islocalhost?'http://localhost:5011':'http://bmscatl.com28.cn';
 const wspath = islocalhost?'/socket.io':'/apisrv/socket.io';
 const organizationid = '599af5dc5f943819f10509e6';
 let config = {
