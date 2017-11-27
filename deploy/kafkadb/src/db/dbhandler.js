6d5da197214f34a7c21bdf0d62d1fc214a543575
wangxiaoqing <xiaoqingwang@126.com>
2017-11-27 15:38:35 +0800

    ===插入报警数据===

--- deploy/kafkadb/src/db/dbhandler.js
+++ deploy/kafkadb/src/db/dbhandler.js
@@ -2,6 +2,9 @@ const mongoose  = require('mongoose');
 // const DBModels = require('../../../restfulsrv/src/db/models.js');
 const DBModels = require('./models.js');
 const _ = require('lodash');
+const alarmplugin = require('../plugins/alarmfilter/index');
+
+
 exports.insertdatatodb= (data,callback)=>{
 
   const LastRealtimeAlarm = _.clone(data.BMSData);
@@ -17,9 +20,9 @@ exports.insertdatatodb= (data,callback)=>{
     devicedata.LastHistoryTrack = LastHistoryTrack;
   }
 
-  console.log(`insertdatatodb LastRealtimeAlarm===>${JSON.stringify(LastRealtimeAlarm)}`);
-  console.log(`insertdatatodb LastHistoryTrack===>${JSON.stringify(LastHistoryTrack)}`);
-  console.log(`insertdatatodb devicedata===>${JSON.stringify(devicedata)}`);
+  // console.log(`insertdatatodb LastRealtimeAlarm===>${JSON.stringify(LastRealtimeAlarm)}`);
+  // console.log(`insertdatatodb LastHistoryTrack===>${JSON.stringify(LastHistoryTrack)}`);
+  // console.log(`insertdatatodb devicedata===>${JSON.stringify(devicedata)}`);
 
   console.log('\n');
 
@@ -34,11 +37,37 @@ exports.insertdatatodb= (data,callback)=>{
   console.log(`start save LastRealtimeAlarm...${!!LastRealtimeAlarm}`);
   if(!!LastRealtimeAlarm){
     LastRealtimeAlarm.DeviceId = devicedata.DeviceId;
-    const dbRealtimeAlarmModel =  DBModels.RealtimeAlarmModel;
-    const entity = new dbRealtimeAlarmModel(LastRealtimeAlarm);
-    entity.save((err,result)=>{
-      console.log(`LastRealtimeAlarm error:${JSON.stringify(err)}`);
+    alarmplugin.dofilter(devicedata.DeviceId,LastRealtimeAlarm.Alarm,(err,result_alarm)=>{
+      console.log(`result_alarm==>${JSON.stringify(result_alarm)}`);
+      if(!err){
+        let updated_data = { $inc: result_alarm.inc_data, CurDay:result_alarm.CurDay,DeviceId:result_alarm.DeviceId,DataTime:LastRealtimeAlarm.DataTime};
+        if(!!LastHistoryTrack){
+          updated_data.Longitude = LastHistoryTrack.Longitude;
+          updated_data.Latitude = LastHistoryTrack.Latitude;
+        }
+        const dbRealtimeAlarmModel =  DBModels.RealtimeAlarmModel;
+        dbRealtimeAlarmModel.findOneAndUpdate({DeviceId:result_alarm.DeviceId,CurDay:result_alarm.CurDay},
+          updated_data,
+          {upsert:true,new: true},(err, result)=> {
+            console.log(`LastRealtimeAlarm error:${JSON.stringify(err)}`);
+          });
+
+          let  result_alarm_raw = LastRealtimeAlarm.Alarm;
+          result_alarm_raw.DeviceId = result_alarm.DeviceId;
+          result_alarm_raw.DataTime = LastRealtimeAlarm.DataTime;
+
+          if(!!LastHistoryTrack){
+            result_alarm_raw.Longitude = LastHistoryTrack.Longitude;
+            result_alarm_raw.Latitude = LastHistoryTrack.Latitude;
+          }
+
+          const entity = new DBModels.RealtimeAlarmRawModel(result_alarm_raw);
+          entity.save((err,result)=>{
+            console.log(`LastRealtimeAlarm error:${JSON.stringify(err)}`);
+          });
+      }
     });
+
   }
 
   console.log(`start save LastHistoryTrack...${!!LastHistoryTrack}`);
--- deploy/kafkadb/src/plugins/alarmfilter/index.js
+++ deploy/kafkadb/src/plugins/alarmfilter/index.js
@@ -1,5 +1,7 @@
 const DBModels = require('../../db/models.js');
 const _ = require('lodash');
+const moment = require('moment');
+
 let alarmrule_systemconfig = {
 
 }
@@ -14,6 +16,44 @@ exports.initplugin = ()=>{
 
 }
 
-exports.dofilter= (alarmdata,callback)=>{
+exports.dofilter= (DeviceId,alarmdata,callback)=>{
   //输入alarmdata,输出：经过处理的alarmdata
+  // "AL_Trouble_Code" : 181
+  //1级：黄色；2级：橙色；3级：红色
+  console.log(`DeviceId==>${JSON.stringify(DeviceId)}`);
+  console.log(`alarmdata==>${JSON.stringify(alarmdata)}`);
+
+  if(!!alarmdata){
+    let inc_data = {};
+    let warninglevel = '';
+    _.map(alarmdata,(v,key)=>{
+      if(key === 'AL_Trouble_Code'){
+        inc_data[`F[${v}]`] = 1;
+      }
+      else{
+        if(v !== 0){
+          inc_data[key] = 1;
+
+          if(v===3){
+            warninglevel = '高';
+          }
+          else if(v===2 && warninglevel!== '高'){
+            warninglevel = '中';
+          }
+          else if(warninglevel!== ''){
+            warninglevel = '低';
+          }
+        }
+      }
+    });
+    const CurDay = moment().format('YYYY-MM-DD');
+    callback(null,{
+      DeviceId,
+      CurDay,
+      inc_data,
+    });
+    return;
+  }
+  callback('无报警数据',null);
+
 }
