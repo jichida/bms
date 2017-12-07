889d5443a00de3822eafb08955c6cb100308df87
wangxiaoqing <xiaoqingwang@126.com>
2017-12-07 22:06:47 +0800

    geo

--- deploy/geodistrict/src/getgeodatas.js
+++ deploy/geodistrict/src/getgeodatas.js
@@ -4,100 +4,162 @@ const utilgeo =  require('./utilgeo.js');
 const db = require('./db/index');
 
 const start = ()=>{
+  utilgeo.get_allgeos((err,result_geo_list)=>{
+    console.log(`获取到数据:${result_geo_list.length}`);
+    if(!err && result_geo_list){
+      _.map(result_geo_list,(result_geo)=>{
+        console.log(`处理数据:${JSON.stringify(result_geo)}`);
+        db.load_geos(result_geo,(result_list)=>{
+          console.log(`从数据库中读取:${JSON.stringify(result_list)}`);
+          let result_geo_rec = result_geo;
+          if(result_list.length === 1){
+            result_geo_rec = result_list[0];
+          }
+          else{
+            db.save_geos(result_geo);
+          }
 
-async.waterfall([
-    (callback)=> {
-      db.load_provices({},(provinces)=>{
-        // console.log(`provinces===>${JSON.stringify(provinces)}`)
-        if(provinces.length === 0){
-          console.log(`provinces数据库中不存在吗`)
-          utilgeo.get_provices((err,provinces)=>{
-            // console.log(`provinces===>${JSON.stringify(provinces)}`)
-            db.save_provices(provinces);
-            callback(null,provinces);
-          });
-        }
-        else{
-          callback(null,provinces);
-        }
-      });
-    },
-    (provinces, callback)=> {
-      // console.log(`isindb:${isindb},provinces===>${JSON.stringify(provinces)}`)
-      let fnsz = [];
-       _.map(provinces,(provice)=>{
-           let fn = (callback)=>{
-            //  console.log(`provinces===>${JSON.stringify(provice)}`);
-             db.load_cities({provice_adcode:provice.adcode},(cities)=>{
-               if(cities.length === 0){//数据库中没有
-                   console.log(`cities数据库中不存在${provice.name}|${provice.adcode}吗`)
-                   utilgeo.get_cities(provice,(err,cities)=>{
-                    //  console.log(`geo 获取cities===>${JSON.stringify(cities)}`);
-                     db.save_cities(cities);
-                     callback(null,cities);
-                   });
+          if(!result_geo.polygons){
+            utilgeo.get_districts_polyline(result_geo,(err,polyline)=>{
+              console.log(`polyline=>${JSON.stringify(polyline)}`)
+              result_geo.polygons = {
+                "type" : "Polygon",
+                "coordinates" : []
+              };
+              result_geo.polygons['coordinates'].push(polyline);
+            });
 
-               }
-               else{
-                  callback(null,cities);
-               }
-             });
-         }
-         fnsz.push(fn);
-       });
-      //  console.log(`函数个数===>${fnsz.length}`);
-       async.parallel(fnsz,(err,result)=>{
-        //  console.log(`async.parallel===>err:${JSON.stringify(err)},${JSON.stringify(result)}`);
-
-         let cities = [];
-         _.map(result,(cts)=>{
-           cities = _.concat(cities,cts);
-         });
-
-         callback(null,cities);
-       });
-    },
-   (cities, callback)=> {
-    //  console.log(`全部城市===>${JSON.stringify(cities)}`);
-     let fnsz = [];
-      _.map(cities,(city)=>{
-        let fn = (callback)=>{
-          db.load_districts({city_adcode:city.adcode},(districts)=>{
-            if(districts.length === 0){
-              console.log(`districts数据库中不存在${city.name}|${city.adcode}吗`)
-              //数据库中不存在
-              utilgeo.get_districts(city,(err,districts)=>{
-                db.save_districts(districts);
-                callback(null,districts);
-              });
-            }
-            else{
-              callback(null,districts);
-            }
-          });
-        }
-        fnsz.push(fn);
-      });
-      // console.log(`函数个数===>${fnsz.length}`);
-      async.parallel(fnsz,(err,result)=>{
-        // console.log(`async.parallel===>err:${JSON.stringify(err)},${JSON.stringify(result)}`);
-        let districts = [];
-        _.map(result,(cts)=>{
-          districts = _.concat(districts,cts);
+            db.update_geos(result_geo);
+          }
         });
-        console.log(`获得全部区域===>${district.length}`);
-        callback(null,districts);
       });
-  },
-  (districts, callback)=> {
-    console.log(`获得全部区域===>${JSON.stringify(districts)}`);
-     _.map(districts,(district)=>{
-       //utilgeo.get_districts_polyline(district,callback);
-     });
- },
-], (err, district_result)=> {
-    // console.log(`district_result===>${JSON.stringify(district_result)}`);
-});
+    }
+
+  });
+
+  // async.waterfall([
+  //     (callback)=> {
+  //       utilgeo.get_allgeos((result_geo_list)=>{
+  //         _.map(result_geo_list,(result_geo)=>{
+  //           db.load_geos(result_geo,(result_list)=>{
+  //             if(result_list.length === 0){
+  //
+  //             }
+  //             else{
+  //
+  //             }
+  //           });
+  //         });
+  //       });
+  //
+  //       db.get_allgeos({},(provinces)=>{
+  //         // console.log(`provinces===>${JSON.stringify(provinces)}`)
+  //         if(provinces.length === 0){
+  //           console.log(`provinces数据库中不存在吗`)
+  //           utilgeo.get_provices((err,provinces)=>{
+  //             // console.log(`provinces===>${JSON.stringify(provinces)}`)
+  //             db.save_provices(provinces);
+  //             callback(null,provinces);
+  //           });
+  //         }
+  //         else{
+  //           callback(null,provinces);
+  //         }
+  //       });
+  //     },
+// async.waterfall([
+//     (callback)=> {
+//       db.load_provices({},(provinces)=>{
+//         // console.log(`provinces===>${JSON.stringify(provinces)}`)
+//         if(provinces.length === 0){
+//           console.log(`provinces数据库中不存在吗`)
+//           utilgeo.get_provices((err,provinces)=>{
+//             // console.log(`provinces===>${JSON.stringify(provinces)}`)
+//             db.save_provices(provinces);
+//             callback(null,provinces);
+//           });
+//         }
+//         else{
+//           callback(null,provinces);
+//         }
+//       });
+//     },
+//     (provinces, callback)=> {
+//       // console.log(`isindb:${isindb},provinces===>${JSON.stringify(provinces)}`)
+//       let fnsz = [];
+//        _.map(provinces,(provice)=>{
+//            let fn = (callback)=>{
+//             //  console.log(`provinces===>${JSON.stringify(provice)}`);
+//              db.load_cities({provice_adcode:provice.adcode},(cities)=>{
+//                if(cities.length === 0){//数据库中没有
+//                    console.log(`cities数据库中不存在${provice.name}|${provice.adcode}吗`)
+//                    utilgeo.get_cities(provice,(err,cities)=>{
+//                     //  console.log(`geo 获取cities===>${JSON.stringify(cities)}`);
+//                      db.save_cities(cities);
+//                      callback(null,cities);
+//                    });
+//
+//                }
+//                else{
+//                   callback(null,cities);
+//                }
+//              });
+//          }
+//          fnsz.push(fn);
+//        });
+//       //  console.log(`函数个数===>${fnsz.length}`);
+//        async.parallel(fnsz,(err,result)=>{
+//         //  console.log(`async.parallel===>err:${JSON.stringify(err)},${JSON.stringify(result)}`);
+//
+//          let cities = [];
+//          _.map(result,(cts)=>{
+//            cities = _.concat(cities,cts);
+//          });
+//
+//          callback(null,cities);
+//        });
+//     },
+//    (cities, callback)=> {
+//     //  console.log(`全部城市===>${JSON.stringify(cities)}`);
+//      let fnsz = [];
+//       _.map(cities,(city)=>{
+//         let fn = (callback)=>{
+//           db.load_districts({city_adcode:city.adcode},(districts)=>{
+//             if(districts.length === 0){
+//               console.log(`districts数据库中不存在${city.name}|${city.adcode}吗`)
+//               //数据库中不存在
+//               utilgeo.get_districts(city,(err,districts)=>{
+//                 db.save_districts(districts);
+//                 callback(null,districts);
+//               });
+//             }
+//             else{
+//               callback(null,districts);
+//             }
+//           });
+//         }
+//         fnsz.push(fn);
+//       });
+//       // console.log(`函数个数===>${fnsz.length}`);
+//       async.parallel(fnsz,(err,result)=>{
+//         // console.log(`async.parallel===>err:${JSON.stringify(err)},${JSON.stringify(result)}`);
+//         let districts = [];
+//         _.map(result,(cts)=>{
+//           districts = _.concat(districts,cts);
+//         });
+//         console.log(`获得全部区域===>${district.length}`);
+//         callback(null,districts);
+//       });
+//   },
+//   (districts, callback)=> {
+//     console.log(`获得全部区域===>${JSON.stringify(districts)}`);
+//      _.map(districts,(district)=>{
+//        //utilgeo.get_districts_polyline(district,callback);
+//      });
+//  },
+// ], (err, district_result)=> {
+//     // console.log(`district_result===>${JSON.stringify(district_result)}`);
+// });
 
 
 };
--- deploy/geodistrict/src/utilgeo.js
+++ deploy/geodistrict/src/utilgeo.js
@@ -3,10 +3,10 @@ const _ = require('lodash');
 
 const key = "604128e90f0f7ca695c811e7ccf5d6f0";
 
-const get_provices = (callback)=>{
+const get_allgeos = (callback)=>{
   const options = {
      method: 'GET',
-     url: `http://restapi.amap.com/v3/config/district?key=${key}&subdistrict=1&keywords=100000`,
+     url: `http://restapi.amap.com/v3/config/district?key=${key}&subdistrict=3&keywords=100000`,
      headers:
       {
         'content-type': 'application/json',
@@ -21,111 +21,170 @@ const get_provices = (callback)=>{
          if(typeof resultobj === 'string'){
              resultobj= JSON.parse(body);
          }
-        //  console.log(`resultobj:${JSON.stringify(resultobj)}`);
+
+         let result_geo = [];
          if(resultobj.status == 1){
            let provinces = [];
-           _.map(resultobj.districts[0].districts,(district)=>{
-             let provice = {
-               adcode:district.adcode,
-               name:district.name,
-               level:district.level,
-             };
-             provinces.push(provice);
+           _.map(resultobj.districts[0].districts,(result_provice)=>{
+            //provinces.push(provice);
+            _.map(result_provice.districts,(result_city)=>{
+              if(result_city.level === 'district' || result_city.districts.length === 0){
+                result_geo.push({
+                  provice_adcode:result_provice.adcode,
+                  provice_name:result_provice.name,
+                  adcode:result_city.adcode,
+                  name:result_city.name,
+                  level:result_city.level
+                });
+              }
+              else{
+                _.map(result_city.districts,(result_district)=>{
+                  result_geo.push({
+                    provice_adcode:result_provice.adcode,
+                    provice_name:result_provice.name,
+                    city_adcode:result_city.adcode,
+                    city_name:result_city.name,
+                    adcode:result_district.adcode,
+                    name:result_district.name,
+                    level:result_district.level
+                  });
+                });
+              }
+            });
            });
-           callback(null,provinces);
-         }
-
+           callback(null,result_geo);
+          //  console.log(`输出结果:${JSON.stringify(result_geo)}`);
+        }
        }
        catch(e){
          console.log(`返回结果e:${JSON.stringify(e)}`);
        }
    });
 }
-
-const get_cities = (provice,callback)=>{
-  console.log(`get_cities 输入参数:${JSON.stringify(provice)}`);
-  const options = {
-     method: 'GET',
-     url: `http://restapi.amap.com/v3/config/district?key=${key}&subdistrict=1&level=province&keywords=${provice.adcode}`,
-     headers:
-      {
-        'content-type': 'application/json',
-         'accept': 'application/json'
-      }
-    };
-   request(options, function (error, response, body) {
-     if (error) throw new Error(error);
-
-       try{
-         let resultobj = body;
-         if(typeof resultobj === 'string'){
-             resultobj= JSON.parse(body);
-         }
-        //  console.log(`get_cities resultobj:${JSON.stringify(resultobj)}`);
-         if(resultobj.status == 1){
-           let cities = [];
-           _.map(resultobj.districts[0].districts,(district)=>{
-             cities.push({
-               provice_adcode:provice.adcode,
-               provice_name:provice.name,
-               adcode:district.adcode,
-               name:district.name,
-               level:district.level,
-             });
-           });
-           callback(null,cities);
-         }
-
-       }
-       catch(e){
-         console.log(`返回结果e:${JSON.stringify(e)}`);
-       }
-   });
-}
-
-const get_districts = (city,callback)=>{
-  console.log(`get_districts 输入参数:${JSON.stringify(city)}`);
-  const options = {
-     method: 'GET',
-     url: `http://restapi.amap.com/v3/config/district?key=${key}&subdistrict=1&level=city&keywords=${city.adcode}`,
-     headers:
-      {
-        'content-type': 'application/json',
-         'accept': 'application/json'
-      }
-    };
-   request(options, function (error, response, body) {
-     if (error) throw new Error(error);
-
-       try{
-         let resultobj = body;
-         if(typeof resultobj === 'string'){
-             resultobj= JSON.parse(body);
-         }
-        //  console.log(`get_districts resultobj:${JSON.stringify(resultobj)}`);
-         if(resultobj.status == 1){
-           let districts = [];
-           _.map(resultobj.districts[0].districts,(district)=>{
-            //  console.log(`get_districts:${JSON.stringify(district)}`);
-             districts.push({
-               provice_adcode:city.provice_adcode,
-               provice_name:city.provice_name,
-               city_adcode:city.adcode,
-               city_name:city.name,
-               adcode:district.adcode,
-               name:district.name,
-               level:district.level,
-             });
-           });
-           callback(null,districts);
-         }
-
-       }
-       catch(e){
-         console.log(`返回结果e:${JSON.stringify(e)}`);
-       }
-   });
-}
+//
+// const get_provices = (callback)=>{
+//   const options = {
+//      method: 'GET',
+//      url: `http://restapi.amap.com/v3/config/district?key=${key}&subdistrict=1&keywords=100000`,
+//      headers:
+//       {
+//         'content-type': 'application/json',
+//          'accept': 'application/json'
+//       }
+//     };
+//    request(options, function (error, response, body) {
+//      if (error) throw new Error(error);
+//
+//        try{
+//          let resultobj = body;
+//          if(typeof resultobj === 'string'){
+//              resultobj= JSON.parse(body);
+//          }
+//         //  console.log(`resultobj:${JSON.stringify(resultobj)}`);
+//          if(resultobj.status == 1){
+//            let provinces = [];
+//            _.map(resultobj.districts[0].districts,(district)=>{
+//              let provice = {
+//                adcode:district.adcode,
+//                name:district.name,
+//                level:district.level,
+//              };
+//              provinces.push(provice);
+//            });
+//            callback(null,provinces);
+//          }
+//
+//        }
+//        catch(e){
+//          console.log(`返回结果e:${JSON.stringify(e)}`);
+//        }
+//    });
+// }
+//
+// const get_cities = (provice,callback)=>{
+//   console.log(`get_cities 输入参数:${JSON.stringify(provice)}`);
+//   const options = {
+//      method: 'GET',
+//      url: `http://restapi.amap.com/v3/config/district?key=${key}&subdistrict=1&level=province&keywords=${provice.adcode}`,
+//      headers:
+//       {
+//         'content-type': 'application/json',
+//          'accept': 'application/json'
+//       }
+//     };
+//    request(options, function (error, response, body) {
+//      if (error) throw new Error(error);
+//
+//        try{
+//          let resultobj = body;
+//          if(typeof resultobj === 'string'){
+//              resultobj= JSON.parse(body);
+//          }
+//         //  console.log(`get_cities resultobj:${JSON.stringify(resultobj)}`);
+//          if(resultobj.status == 1){
+//            let cities = [];
+//            _.map(resultobj.districts[0].districts,(district)=>{
+//              cities.push({
+//                provice_adcode:provice.adcode,
+//                provice_name:provice.name,
+//                adcode:district.adcode,
+//                name:district.name,
+//                level:district.level,
+//              });
+//            });
+//            callback(null,cities);
+//          }
+//
+//        }
+//        catch(e){
+//          console.log(`返回结果e:${JSON.stringify(e)}`);
+//        }
+//    });
+// }
+//
+// const get_districts = (city,callback)=>{
+//   console.log(`get_districts 输入参数:${JSON.stringify(city)}`);
+//   const options = {
+//      method: 'GET',
+//      url: `http://restapi.amap.com/v3/config/district?key=${key}&subdistrict=1&level=city&keywords=${city.adcode}`,
+//      headers:
+//       {
+//         'content-type': 'application/json',
+//          'accept': 'application/json'
+//       }
+//     };
+//    request(options, function (error, response, body) {
+//      if (error) throw new Error(error);
+//
+//        try{
+//          let resultobj = body;
+//          if(typeof resultobj === 'string'){
+//              resultobj= JSON.parse(body);
+//          }
+//         //  console.log(`get_districts resultobj:${JSON.stringify(resultobj)}`);
+//          if(resultobj.status == 1){
+//            let districts = [];
+//            _.map(resultobj.districts[0].districts,(district)=>{
+//             //  console.log(`get_districts:${JSON.stringify(district)}`);
+//              districts.push({
+//                provice_adcode:city.provice_adcode,
+//                provice_name:city.provice_name,
+//                city_adcode:city.adcode,
+//                city_name:city.name,
+//                adcode:district.adcode,
+//                name:district.name,
+//                level:district.level,
+//              });
+//            });
+//            callback(null,districts);
+//          }
+//
+//        }
+//        catch(e){
+//          console.log(`返回结果e:${JSON.stringify(e)}`);
+//        }
+//    });
+// }
 
 
 const get_districts_polyline = (district,callback)=>{
@@ -149,18 +208,18 @@ const get_districts_polyline = (district,callback)=>{
          }
         //  console.log(`get_districts resultobj:${JSON.stringify(resultobj)}`);
          if(resultobj.status == 1){
-           let district_result = {
-             provice_adcode:city.provice_adcode,
-             provice_name:city.provice_name,
-             city_adcode:city.city_adcode,
-             city_name:city.city_name,
-             district_adcode:district.adcode,
-             district_name:district.name,
-             level:district.level,
-           };
            const polyline_str = resultobj.districts[0].polyline;
-           district_result.polyline = polyline_str.split(';');
-           callback(null,district_result);
+           let polyline = [];
+           let polylinesz = polyline_str.split(';');
+           _.map(polylinesz,(pzstr)=>{
+             let locz = pzstr.split(',');
+             if(locz.length === 2){
+               let lng = parseFloat(locz[0]);
+               let lat = parseFloat(locz[1]);
+               polyline.push([lng,lat]);
+             }
+           });
+           callback(null,polyline);
          }
 
        }
@@ -193,8 +252,12 @@ const get_districts_polyline = (district,callback)=>{
 //     })
 //   }
 // });
-
-exports.get_provices = get_provices;
-exports.get_cities = get_cities;
-exports.get_districts = get_districts;
+const district = {"provice_adcode":"330000","provice_name":"浙江省","city_adcode":"330900","city_name":"舟山市","adcode":"330902","name":"定海区","level":"district"}
+get_districts_polyline(district,(err,polyline)=>{
+  console.log(`polyline=>${JSON.stringify(polyline)}`)
+});
+// exports.get_provices = get_provices;
+// exports.get_cities = get_cities;
+// exports.get_districts = get_districts;
 exports.get_districts_polyline = get_districts_polyline;
+exports.get_allgeos = get_allgeos;
