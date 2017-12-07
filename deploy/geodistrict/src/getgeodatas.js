c029a620e38ffbd512c197a9af94c4fc2aae684d
wangxiaoqing <xiaoqingwang@126.com>
2017-12-07 17:20:56 +0800

    全部区域获得

--- deploy/geodistrict/src/getgeodatas.js
+++ deploy/geodistrict/src/getgeodatas.js
@@ -61,27 +61,40 @@ async.waterfall([
     },
    (cities, callback)=> {
      console.log(`全部城市===>${JSON.stringify(cities)}`);
+     let fnsz = [];
       _.map(cities,(city)=>{
-        if(city.level === 'district'){
-        }
-        else{
-          db.load_onecity(city,(result)=>{
-            if(!result && city.level === 'city'){
-              //数据库中无城市数据
-              utilgeo.get_districts(city,callback);
+        let fn = (callback)=>{
+          db.load_districts({adcode:city.city_adcode},(districts)=>{
+            if(districts.length === 0){
+              //数据库中不存在
+              utilgeo.get_districts(city,(err,districts)=>{
+                db.save_districts(districts);
+                callback(null,districts);
+              });
+            }
+            else{
+              callback(null,districts);
             }
           });
         }
+        fnsz.push(fn);
+      });
+      console.log(`函数个数===>${fnsz.length}`);
+      async.parallel(fnsz,(err,result)=>{
+        console.log(`async.parallel===>err:${JSON.stringify(err)},${JSON.stringify(result)}`);
 
-        // if(city.adcode === '320400'){
-        //   utilgeo.get_districts(city,callback);
-        // }
+        let districts = [];
+        _.map(result,(cts)=>{
+          districts = _.concat(districts,cts);
+        });
+
+        callback(null,districts);
       });
   },
   (districts, callback)=> {
-    console.log(`districts===>${JSON.stringify(districts)}`);
+    console.log(`获得全部区域===>${JSON.stringify(districts)}`);
      _.map(districts,(district)=>{
-       utilgeo.get_districts_polyline(district,callback);
+       //utilgeo.get_districts_polyline(district,callback);
      });
  },
 ], (err, district_result)=> {
@@ -91,27 +104,3 @@ async.waterfall([
 
 };
 exports.start = start;
-// get_provices((err,provinces)=>{
-//   console.log(`所有省份:${JSON.stringify(provinces)}`);
-//   if(!err && !!provinces){
-//     _.map(provinces,(provice)=>{
-//       get_cities(provice,(err,cities)=>{
-//         console.log(`所有城市:${JSON.stringify(cities)}`);
-//         _.map(cities,(err,city)=>{
-//           get_districts(city,(err,districts)=>{
-//             console.log(`所有区域:${JSON.stringify(districts)}`);
-//           });
-//         });
-//       });
-//     });
-//   }
-// });
-// const city = {"provice_adcode":"500000","provice_name":"重庆市","adcode":"500100","name":"重庆城区"};
-// get_districts(city,(err,districts)=>{
-//   console.log(`所有区域:${JSON.stringify(districts)}`);
-//   if(districts.length > 0){
-//     get_districts_polyline(districts[0],(err,district_result)=>{
-//       console.log(`district_result===>${JSON.stringify(district_result)}`);
-//     })
-//   }
-// });
