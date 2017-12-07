50dc790ed39b1aef32aa9951c228419914b906aa
wangxiaoqing <xiaoqingwang@126.com>
2017-12-08 00:46:46 +0800

    失败

--- deploy/geodistrict/src/getgeodatas.js
+++ deploy/geodistrict/src/getgeodatas.js
@@ -6,95 +6,115 @@ const geolib = require('geolib');
 const moment = require('moment');
 
 const start = ()=>{
+  const insert_db = ()=>{
+    utilgeo.get_allgeos((err,result_geo_list)=>{
+      console.log(`获取到数据:${result_geo_list.length}`);
+      if(!err && result_geo_list){
+        _.map(result_geo_list,(result_geo)=>{
+          console.log(`处理数据:${JSON.stringify(result_geo)}`);
+          //setTimeout(()=>{
+            db.load_geos(result_geo,(result_list)=>{
+              // console.log(`从数据库中读取:${JSON.stringify(result_list)}`);
+              let result_geo_rec = result_geo;
+              if(result_list.length === 1){
+                result_geo_rec = result_list[0];
+              }
+              else{
+                db.save_geos(result_geo);
+              }
 
-  // utilgeo.get_allgeos((err,result_geo_list)=>{
-  //   console.log(`获取到数据:${result_geo_list.length}`);
-  //   if(!err && result_geo_list){
-  //     _.map(result_geo_list,(result_geo)=>{
-  //       console.log(`处理数据:${JSON.stringify(result_geo)}`);
-  //       //setTimeout(()=>{
-  //         db.load_geos(result_geo,(result_list)=>{
-  //           // console.log(`从数据库中读取:${JSON.stringify(result_list)}`);
-  //           let result_geo_rec = result_geo;
-  //           if(result_list.length === 1){
-  //             result_geo_rec = result_list[0];
-  //           }
-  //           else{
-  //             db.save_geos(result_geo);
-  //           }
-  //
-  //           if(!result_geo_rec.polygons){
-  //             console.log(`找不到 polyline=>${JSON.stringify(result_geo_rec)}`)
-  //             utilgeo.get_districts_polyline(result_geo,(err,polyline)=>{
-  //               console.log(`polyline=>${JSON.stringify(polyline)}`)
-  //               result_geo.polygons = {
-  //                 "type" : "Polygon",
-  //                 "coordinates" : []
-  //               };
-  //               result_geo.polygons['coordinates'].push(polyline);
-  //               db.update_geos(result_geo);
-  //             });
-  //           }
-  //         });
-  //       // },100);//timeout
-  //
-  //     });
-  //   }
+              if(!result_geo_rec.polygons){
+                // console.log(`找不到 polyline=>${JSON.stringify(result_geo_rec)}`)
+                utilgeo.get_districts_polyline(result_geo,(err,polyline)=>{
+                  console.log(`polyline=>${JSON.stringify(polyline)}`)
+                  result_geo.polygons = {
+                    "type" : "Polygon",
+                    "coordinates" : []
+                  };
+                  result_geo.polygons['coordinates'].push(polyline);
+                  db.update_geos(result_geo);
+                });
+              }
+            });
+          // },100);//timeout
 
-  // });
-
-
-let maplist =[];
-db.load_geos({"adcode" : "450202"},(result_geo_list)=>{
-  console.log(`获取到数据:${result_geo_list.length}`);
-  if(result_geo_list.length > 0){
-    _.map(result_geo_list,(result_geo)=>{
-      if(!!result_geo.adcode){
-        let array_polygon = [];
-        let polygons_list = result_geo.polygons['coordinates'][0];
-        let result_geo_new = _.omit(result_geo,'coordinates');
-        _.map(polygons_list,(pt)=>{
-          array_polygon.push({
-            latitude:pt[1],
-            longitude:pt[0]
-          });
         });
-        console.log(`array_polygon:${JSON.stringify(array_polygon)}`)
-        result_geo_new.array_polygon = array_polygon;
-        maplist.push(result_geo_new);
-        // mapsz[result_geo.adcode] = result_geo_new;
       }
-      else{
-        console.log(`mapsz[result_geo.adcode]???===>${JSON.stringify(result_geo)}`)
-      }
+
     });
-
-
-    const getgeofrompoint = ((point)=>{
-      let matched = {};
-      _.map(maplist,(v,k)=>{
-        console.log(`v.array_polygon???===>${JSON.stringify(v.array_polygon)}`)
-        if(geolib.isPointInside(point,v.array_polygon)){
-          return v;
-        }
-      });
-      return null;
-    });
-
-    console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')}==>${maplist.length}`);
-    let point = {latitude: 40.2207700000, longitude: 116.2312800000};
-    let result = getgeofrompoint(point);
-    console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')}判断结果${JSON.stringify(result)}`);
-    point = {latitude: 24.350483, longitude: 109.433671};
-    result = getgeofrompoint(point);
-    console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')}判断结果${JSON.stringify(result)}`);
-    point = {latitude: 26.258009, longitude: 119.378868};
-    result = getgeofrompoint(point);
-    console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')}判断结果${JSON.stringify(result)}`);
   }
-});
+
+  let test_ptinpolygons = ()=>{
+
+    let maplist =[];
+    db.load_geos({"provice_adcode" : "110000"},(result_geo_list)=>{
+      // console.log(`获取到数据:${result_geo_list.length}`);
+      if(result_geo_list.length > 0){
+        _.map(result_geo_list,(result_geo)=>{
+          if(!!result_geo.adcode){
+            let array_polygon = [];
+            let polygons_list = result_geo.polygons['coordinates'][0];
+            let result_geo_new = _.omit(result_geo,'coordinates');
+            _.map(polygons_list,(pt)=>{
+              array_polygon.push({
+                latitude:pt[1],
+                longitude:pt[0]
+              });
+            });
+            // console.log(`array_polygon:${JSON.stringify(array_polygon)}`)
+            result_geo_new.array_polygon = array_polygon;
+            maplist.push(result_geo_new);
+            // mapsz[result_geo.adcode] = result_geo_new;
+          }
+          else{
+            console.log(`mapsz[result_geo.adcode]???===>${JSON.stringify(result_geo)}`)
+          }
+        });
 
 
+        const getgeofrompoint = ((point)=>{
+          let matched = {};
+          _.map(maplist,(v,k)=>{
+            // console.log(`v.array_polygon???===>${JSON.stringify(v.array_polygon)}`)
+            if(geolib.isPointInside(point,v.array_polygon)){
+              return v;
+            }
+          });
+          return null;
+        });
+
+        console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')}==>${maplist.length}`);
+        let point = {latitude: 40.2207700000, longitude: 116.2312800000};
+        let result = getgeofrompoint(point);
+        console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')}判断结果${JSON.stringify(result)}`);
+        point = {latitude: 24.350483, longitude: 109.433671};
+        result = getgeofrompoint(point);
+        console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')}判断结果${JSON.stringify(result)}`);
+        point = {latitude: 26.258009, longitude: 119.378868};
+        result = getgeofrompoint(point);
+        console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')}判断结果${JSON.stringify(result)}`);
+      }
+    });
+  }
+
+  test_ptinpolygons();
+// console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')}==>`);
+// let query =
+// {
+//   polygons:
+//      {
+//        $geoIntersects:
+//          {
+//            $geometry:{
+//               "type" : "Point",
+//               "coordinates" : [116.2312800000,40.2207700000]
+//             }
+//           }
+//       }
+//  };
+//  db.load_geos(query,(result_geo_list)=>{
+//    console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')}==>获取到数据:${JSON.stringify(result_geo_list)}`);
+//  });
   // async.waterfall([
   //     (callback)=> {
   //       utilgeo.get_allgeos((result_geo_list)=>{
