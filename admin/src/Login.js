27f1d5e6c27f1962ee55eaee1e594498e37d6b88
Jiaowenhui <523947655@qq.com>
2017-12-25 15:08:49 +0800

    版本号

--- admin/src/Login.js
+++ admin/src/Login.js
@@ -11,6 +11,7 @@ import Avatar from 'material-ui/Avatar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import LockIcon from 'material-ui/svg-icons/action/lock-outline';
+import Config from './env/config';
 import { cyan500, pinkA200 } from 'material-ui/styles/colors';
 
 import { Notification, translate, userLogin as userLoginAction } from 'admin-on-rest';
@@ -88,7 +89,7 @@ class Login extends Component {
                                         name="username"
                                         component={renderInput}
                                         floatingLabelText={translate('aor.auth.username')}
-                                    />
+                                        />
                                 </div>
                                 <div style={styles.input}>
                                     <Field
@@ -96,12 +97,15 @@ class Login extends Component {
                                         component={renderInput}
                                         floatingLabelText={translate('aor.auth.password')}
                                         type="password"
-                                    />
+                                        />
                                 </div>
                             </div>
                             <CardActions>
                                 <RaisedButton type="submit" primary disabled={submitting} label={translate('aor.auth.sign_in')} fullWidth />
                             </CardActions>
+
+
+                            <p style={{textAlign:"center",padding:"10px 0;"}}>版本号：{Config.appversion}</p>
                         </form>
                     </Card>
                     <Notification />
