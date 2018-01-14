c279010c2feecc775d248830926cb24c3633102f
wangxiaoqing <xiaoqingwang@126.com>
2018-01-14 10:11:09 +0800

    ==

--- pc/src/env/config.dev.js
+++ pc/src/env/config.dev.js
@@ -1,4 +1,4 @@
-let islocalhost = true;
+let islocalhost = false;
 const serverurl = islocalhost?'http://localhost:5011':'http://bms.com28.cn';
 const serverurlrestful = islocalhost?`${serverurl}/api`:`${serverurl}/apisrv/api`;
 const wspath = islocalhost?'/socket.io':'/apisrv/socket.io';
@@ -11,7 +11,7 @@ let config = {
     requesttimeout:5000,
     appversion:'1.2.1',
     sendlocationinterval:20000,
-    softmode:'app'
+    softmode:'pc'
 };
 
 
