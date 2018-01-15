2db9145d7d7413d223f4750a59f5b6e390343bd4
wangxiaoqing <xiaoqingwang@126.com>
2018-01-15 18:20:28 +0800

    推送啦

--- deploy/restfulsrv/src/handler/socketsubscribe.js
+++ deploy/restfulsrv/src/handler/socketsubscribe.js
@@ -2,18 +2,36 @@ const winston = require('../log/log.js');
 const _ =  require('lodash');
 const config = require('../config.js');
 const realtimealarm = require('./common/realtimealarm');
+const smspush = require('../smspush/push');
 
 const pushusermessage = (socket,ctx,DeviceId,data)=>{
   console.log(`开始推送了,注意啊----->${JSON.stringify(data)}`);
+
+  let recordnew = realtimealarm.bridge_alarminfo(data);
   if(ctx.usertype === 'pc'){
-    let recordnew = realtimealarm.bridge_alarminfo(data);
     // recordnew = _.omit(recordnew,['key']);
     console.log(`recordnew===>${JSON.stringify(recordnew)}`);
     // {"key":"5a1bfa0f46a68b00019fd34b","车辆ID":"1639101505","报警时间":"2017-11-17 22:20:42","报警信息":"故障代码 179次|"}
     socket.emit('serverpush_alarm',recordnew);
   }
   else{
+    // _id
+    // messagetype:String,//all,app
+    // touserid:String,
+    // messagetitle:String,
+    // messagecontent:String,
     //推送到app,调用接口|serverpush_alarm
+    const messagenotify = {
+      _id:recordnew.key,
+      messagetype:'app',
+      touserid:ctx.userid.toString(),
+      messagetitle:`车辆:${recordnew['车辆ID']}于${recordnew['报警时间']}报警,报警信息:${recordnew['报警信息']}`,
+      messagecontent:`车辆:${recordnew['车辆ID']}于${recordnew['报警时间']}报警,报警信息:${recordnew['报警信息']}`
+    };
+    smspush.sendnotifymessage(messagenotify,(err,result)=>{
+      console.log(err);
+      console.log(result);
+    });
   }
 }
 
