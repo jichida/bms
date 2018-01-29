1d4867e1dd38ff0549ff49adb31e6a75748cad67
wangxiaoqing <xiaoqingwang@126.com>
2018-01-29 16:21:17 +0800

    设置报警规则

--- deploy/kafkadb/src/plugins/alarmfilter/index.js
+++ deploy/kafkadb/src/plugins/alarmfilter/index.js
@@ -1,22 +1,157 @@
 const DBModels = require('../../db/models.js');
 const _ = require('lodash');
 const moment = require('moment');
+const mongoose     = require('mongoose');
+// let alarmrule_systemconfig = {
+//
+// }
+//
+// exports.initplugin = ()=>{
+//   const systemconfigModel = DBModels.SystemConfigModel;
+//   systemconfigModel.findOne({ organizationid: organizationid }, (err, systemconfig)=> {
+//     if(!err && !!systemconfig){
+//       alarmrule_systemconfig = systemconfig;
+//     }
+//   });
+//
+// }
 
-let alarmrule_systemconfig = {
-
-}
-
-exports.initplugin = ()=>{
-  const systemconfigModel = DBModels.SystemConfigModel;
-  systemconfigModel.findOne({ organizationid: organizationid }, (err, systemconfig)=> {
-    if(!err && !!systemconfig){
-      alarmrule_systemconfig = systemconfig;
+const getalarmrules = (systemconfig)=>{
+  let alarmrules = {};
+  _.map(systemconfig.warningrulelevel0,(v)=>{
+    if(!!alarmrules[v.name]){
+      alarmrules[v.name].push({
+        rule:v,
+        warninglevel:'高'
+      });
+    }
+    else{
+      alarmrules[v.name] = [{
+        rule:v,
+        warninglevel:'高'
+      }];
     }
   });
-
+  _.map(systemconfig.warningrulelevel1,(v)=>{
+    if(!!alarmrules[v.name]){
+      alarmrules[v.name].push({
+        rule:v,
+        warninglevel:'中'
+      });
+    }
+    else{
+      alarmrules[v.name] = [{
+        rule:v,
+        warninglevel:'中'
+      }];
+    }
+  });
+  _.map(systemconfig.warningrulelevel2,(v)=>{
+    if(!!alarmrules[v.name]){
+      alarmrules[v.name].push({
+        rule:v,
+        warninglevel:'低'
+      });
+    }
+    else{
+      alarmrules[v.name] = [{
+        rule:v,
+        warninglevel:'低'
+      }];
+    }
+  });
+  return alarmrules;
 }
 
-exports.dofilter= (DeviceId,LastRealtimeAlarm,callback)=>{
+const getresultalarmmatch = (alarmdata,alarmrules)=>{
+  let resultalarmmatch = [];
+  _.map(alarmdata,(v,key)=>{
+    //"AL_Trouble_Code" : 181
+    if(!!alarmrules[key]){
+      _.map(alarmrules[key],(valarmrules)=>{
+        const rule = valarmrules.rule;
+        // {
+        //     "content" : "故障228代码报警",
+        //     "value" : "228",
+        //     "op" : "=",
+        //     "name" : "AL_Trouble_Code"
+        // }
+        let valueint = rule.value;
+        try{
+          if(typeof valueint === 'string'){
+            valueint = parseFloat(valueint);
+          }
+        }
+        catch(e){
+
+        }
+
+        if(rule.op === '='){
+          if(valueint == v){
+            resultalarmmatch.push({
+              warninglevel:valarmrules.warninglevel,
+              alarmtxt:rule.content
+            });
+          }
+        }
+        else if(rule.op === '>'){
+          if(v > valueint){
+            resultalarmmatch.push({
+              warninglevel:valarmrules.warninglevel,
+              alarmtxt:rule.content
+            });
+          }
+        }
+        else if(rule.op === '<'){
+          if(v < valueint){
+            resultalarmmatch.push({
+              warninglevel:valarmrules.warninglevel,
+              alarmtxt:rule.content
+            });
+          }
+        }//else if
+      });//_.map(alarmrules[key],(valarmrules)=>{
+    };//if(!!alarmrules[key]){
+  });//_.map(alarmdata,(v,key)=>{
+  return resultalarmmatch;
+}
+
+const matchalarm = (alarmdata,callback)=>{
+    if(!!alarmdata){
+      const systemconfigModel = DBModels.SystemConfigModel;
+      systemconfigModel.findOne({ organizationid: mongoose.Types.ObjectId("599af5dc5f943819f10509e6") }, (err, result)=> {
+          let resultalarmmatch = [];
+          if(!err && !!result){
+            let systemconfig = result.toJSON();
+            let alarmrules = getalarmrules(systemconfig);
+            console.log(alarmrules);
+            let resultalarmmatch = getresultalarmmatch(alarmdata,alarmrules);
+
+            console.log(resultalarmmatch);
+            resultalarmmatch = _.sortBy(resultalarmmatch,(v)=>{
+              if(v.warninglevel === '高'){
+                return 0;
+              }
+              if(v.warninglevel === '中'){
+                return 1;
+              }
+              if(v.warninglevel === '低'){
+                return 2;
+              }
+              return 3;
+            });
+
+            console.log(resultalarmmatch);
+          }
+          callback(resultalarmmatch);
+      });
+    }
+    else{
+      callback([]);
+    }
+}
+
+const dofilter= (DeviceId,LastRealtimeAlarm,callback)=>{
   //输入alarmdata,输出：经过处理的alarmdata
   // "AL_Trouble_Code" : 181
   //1级：黄色；2级：橙色；3级：红色
@@ -60,3 +195,6 @@ exports.dofilter= (DeviceId,LastRealtimeAlarm,callback)=>{
   callback('无报警数据',null);
 
 }
+
+exports.matchalarm = matchalarm;
+exports.dofilter = dofilter;
