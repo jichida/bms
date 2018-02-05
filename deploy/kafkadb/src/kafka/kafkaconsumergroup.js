c1f7051ac269c95b55cf4e645652a8a9965a48b5
wangxiaoqing <xiaoqingwang@126.com>
2018-02-05 23:50:57 +0800

    consumerGroup

--- deploy/kafkadb/src/kafka/kafkaconsumergroup.js
+++ deploy/kafkadb/src/kafka/kafkaconsumergroup.js
@@ -1,13 +1,13 @@
 const async = require('async');
 const ConsumerGroup = require('kafka-node').ConsumerGroup;
-const uuid = require('uuid');
-const cid = uuid.v4();
+// const uuid = require('uuid');
+// const cid = uuid.v4();
 const config = require('../config');
 
 const startsrv = (config,onMessage,onError)=>{
   const consumerOptions = config.consumerOptions;
   const topics = config.consumertopics;
-  const consumerGroup = new ConsumerGroup(Object.assign({id: cid}, consumerOptions), topics);
+  const consumerGroup = new ConsumerGroup(Object.assign({id: config.NodeID}, consumerOptions), topics);
   consumerGroup.on('error', onError);
   consumerGroup.on('message', onMessage);
   console.log(`等待消息${config.NodeID}`);
