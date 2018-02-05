086802a7a4e37fdf88e3e99bbfdcb157ae78f476
wangxiaoqing <xiaoqingwang@126.com>
2018-02-05 18:02:53 +0800

    config

--- deploy/LogServer/src/config.js
+++ deploy/LogServer/src/config.js
@@ -1,5 +1,4 @@
 let config =  {
-  mongodburl:process.env.MONGO_URL || 'mongodb://localhost/bms',
   consumerOptions:{
     host: process.env.KAFKA_HOST ||'118.31.41.232:2181',
     mongos:process.env.mongos==='true'?true:false,
@@ -8,8 +7,6 @@ let config =  {
     protocol: ['roundrobin'],
     fromOffset: 'earliest' // equivalent of auto.offset.reset valid values are 'none', 'latest', 'earliest'
   },
-  mapdict:{},
-  NodeID:process.env.NodeID || 1,
   kafka_pushalaramtopic:'push.alarm',
   kafka_bmslogtopic:'log.bms',
   consumertopics:['BMS.Data']
