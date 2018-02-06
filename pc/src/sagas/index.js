bb6df0c42634d48d619cafd7399139def3cae94c
wangxiaoqing <xiaoqingwang@126.com>
2018-02-06 14:47:44 +0800

    createload

--- pc/src/sagas/index.js
+++ pc/src/sagas/index.js
@@ -24,7 +24,7 @@ export default function* rootSaga() {
     yield fork(socketflow);
     yield fork(createmapmainflow);
     yield fork(createmaptrackhistoryplaybackflow);
-    yield fork(createloadingflow);
+    // yield fork(createloadingflow);
     yield fork(uiflow);
     yield fork(wsrecvsagaflow);
     yield fork(createsagacallbackflow);
