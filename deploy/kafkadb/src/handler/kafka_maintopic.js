911f11a417107e07f7c42f53c0131817d0931a50
wangxiaoqing <xiaoqingwang@126.com>
2018-02-07 00:11:17 +0800

    data

--- deploy/kafkadb/src/handler/kafka_maintopic.js
+++ deploy/kafkadb/src/handler/kafka_maintopic.js
@@ -4,6 +4,8 @@ const sendtokafka = require('../kafka/sendtokafka');
 //接受BMS.data=>原样广播成bms.index
 
 const kafka_maintopic = (data,callback)=>{
+  console.log(`kafka_maintopic,${config.NodeID}】接收成功${data.SN64},${data.DeviceId}`);
+
   const sendto = sendtokafka.getsendtokafka();
   if(!!sendto){
     sendto(data,config.kafka_dbtopic_index,(err,data)=>{
