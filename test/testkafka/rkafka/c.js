06ca8d2b8c63b470a5743cfbcea2a05627da6bfb
wangxiaoqing <xiaoqingwang@126.com>
2018-02-13 01:03:45 +0800

    testrkafka

---
+++ test/testkafka/rkafka/c.js
@@ -0,0 +1,27 @@
+const kafka = require('node-rdkafka');
+
+const initConsumer =(globalconfig,cconfig,onMsg,onErr)=> {
+  return new Promise((resolve, reject) => {
+    const consumer = new kafka.KafkaConsumer(globalconfig,cconfig);
+
+    consumer.on('ready', () => {
+      console.log('consumer ready.');
+      consumer.subscribe(topics || ['bms.index'])
+      consumer.consume();
+      resolve(consumer)
+    })
+    consumer.on('data', (msg)=> {
+      onMsg(msg);
+    });
+    consumer.on('event.error', onErr);
+    consumer.on('disconnected', () => {
+      process.exit(0)
+    });
+    consumer.on('event.log', (event)=> {
+      console.log(event)
+    })
+    consumer.connect();
+  });
+}
+
+module.exports = initConsumer;
---
+++ test/testkafka/rkafka/p.js
@@ -0,0 +1,25 @@
+const kafka = require('node-rdkafka');
+
+
+const initProducer = (globalconfig,pconfig,onErr)=> {
+  return new Promise((resolve, reject) => {
+    const producer = new kafka.Producer(globalconfig,pconfig);
+    producer.on('ready', () => {
+      console.log('producer ready.');
+      producer.setPollInterval(500)
+      resolve(producer)
+    })
+    producer.on('event.error', onErr)
+    producer.on('disconnected', () => {
+      console.log('producer disconnected.');
+      process.exit(0)
+    });
+    producer.on('event.log', function(event) {
+      console.log(event)
+    })
+    producer.connect();
+  });
+}
+
+
+module.exports = initProducer;
