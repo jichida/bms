7c16cf7a53ed0ae8a2b3204fcc3676b6175fc7c3
wangxiaoqing <xiaoqingwang@126.com>
2018-02-14 00:45:29 +0800

    no message

--- deploy/kafkadb/src/config.js
+++ deploy/kafkadb/src/config.js
@@ -1,10 +1,11 @@
+const nodeid = process.env.NodeID || 1;
 let config =  {
   mongodburl:process.env.MONGO_URL || 'mongodb://localhost/bms',
   mongos:process.env.mongos==='true'?true:false,
   kafka_cconfig1:{
       'group.id': process.env.GroupId ||'bmsgroup',
       'metadata.broker.list': process.env.KAFKA_HOST || '192.168.1.20:9092,192.168.1.114:9092,192.168.1.136:9092',
-      'client.id':`c${process.pid}_${process.env.NodeID || 1}`,
+      'client.id':`c${process.pid}_${nodeid}`,
       'partition.assignment.strategy':'roundrobin',
       'enable.auto.commit': false
   },
@@ -17,7 +18,7 @@ let config =  {
   kafka_pconfig2:{
   },
   mapdict:{},
-  NodeID:process.env.NodeID || 1,
+  NodeID:nodeid,
   partitionnumber:!!process.env.partitionnumber?parseInt(process.env.partitionnumber):48,
   jpush_appkey:process.env.jpush_appkey || '630950d8101fe73d19d64aaf',
   jpush_mastersecret:process.env.jpush_mastersecret || 'dd52bf9de919a2a53441fce3',
