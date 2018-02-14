fe50bd086120cc1a8b541093da0bc2efc7c1abd1
wangxiaoqing <xiaoqingwang@126.com>
2018-02-15 01:01:16 +0800

    100 number

--- deploy/kafkadb/src/kafka/kc.js
+++ deploy/kafkadb/src/kafka/kc.js
@@ -6,7 +6,7 @@ const moment = require('moment');
 const uuid = require('uuid');
 const kafkadb = require('../handler/kafkadb.js');
 
-const numMessages = 1000;
+const numMessages = 100;
 
 const processbatchmsgs = (msgs,callbackfnmsg)=>{
   const msgid = uuid.v4();
