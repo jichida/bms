8216d41815e750123bc7d84cf0c2ba5254321847
wangxiaoqing <xiaoqingwang@126.com>
2018-02-15 22:16:07 +0800

    config

--- admin/src/env/config.prod.js
+++ admin/src/env/config.prod.js
@@ -1,5 +1,5 @@
 let islocalhost = false;
-let serverurl = islocalhost?'http://localhost:5011':'http://101.89.141.109:81/apisrv';
+let serverurl = islocalhost?'http://localhost:5011':'http://bmscatl.com28.cn/apisrv';
 const organizationid = '599af5dc5f943819f10509e6';
 export default {
     restserverurl:`${serverurl}/adminapi/v1/${organizationid}`,
--- pc/src/env/config.prod.js
+++ pc/src/env/config.prod.js
@@ -1,5 +1,5 @@
 let islocalhost = false;
-const serverurl = islocalhost?'http://localhost:5011':'http://101.89.141.109:81';
+const serverurl = islocalhost?'http://localhost:5011':'http://bmscatl.com28.cn/';
 const serverurlrestful = islocalhost?`${serverurl}/api`:`${serverurl}/apisrv/api`;
 const wspath = islocalhost?'/socket.io':'/apisrv/socket.io';
 const organizationid = '599af5dc5f943819f10509e6';
