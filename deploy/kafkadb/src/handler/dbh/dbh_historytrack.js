56847c9abcda145ba4ac407c5a1d0da9dbf61176
wangxiaoqing <xiaoqingwang@126.com>
2018-03-03 01:56:19 +0800

    http://cfc.kizzx2.com/index.php/slow-batch-insert-with-mongodb-sharding-and-how-i-debugged-it/ So I wrote a quick script (included below) to try it on my MacBook Pro with SSD, and I was able to get results like this:
    20000 documents inserted
    Took 580ms
    34482.75862068966 doc/s
    So something must be wrong with his configuration / code, I thought, and I kept telling him to just run my code on his machine.
    
    Turns out it’s due to MongoDB sharding failing with batch inserts
    So it turns out performance dropped drastically also for me after I enabled sharding:
    
    20000 documents inserted
    Took 15701ms
    1273.8042162919558 doc/s
    The test set up
    
    Here’s what I used to test:
    
    3 config servers
    8 shards, sharding on { _id: "hashed" }
    All on localhost
    Here is the setup script I used to create 8 shards on my localhost. (By the way, setting up sharding is painful)

--- deploy/kafkadb/src/handler/dbh/dbh_historytrack.js
+++ deploy/kafkadb/src/handler/dbh/dbh_historytrack.js
@@ -1,38 +1,55 @@
 const DBModels = require('../models.js');
 const _ = require('lodash');
 const debug_historytrack = require('debug')('dbh:historytrack');
+const async = require('async');
 
 const dbh_historytrack =(datas,callbackfn)=>{
   const dbModel = DBModels.HistoryTrackModel;
   debug_historytrack(`start dbh_historytrack`);
-  const bulk = dbModel.collection.initializeUnorderedBulkOp();
-  if(!!bulk){
-    _.map(datas,(devicedata)=>{
-      bulk.insert(devicedata);
-      // bulk.find({
-      //     GUID:devicedata.GUID,
-      //     DeviceId:devicedata.DeviceId,
-      //     GPSTime:devicedata.GPSTime,
-      //   })
-      //   .upsert()
-      //   .updateOne({
-      //     $set:devicedata
-      //   });
-    });
-    bulk.execute((err,result)=>{
-      if(!!err){
-        console.error(`dbh_historytrack err`);
-        console.error(err);
-        console.error(err.stack);
+  const asyncfnsz = [];
+  _.map(datas,(devicedata)=>{
+    asyncfnsz.push(
+      (callbackfn)=>{
+        dbModel.findOneAndUpdate({
+              GUID:devicedata.GUID,
+              DeviceId:devicedata.DeviceId,
+              GPSTime:devicedata.GPSTime,
+         },{$set:devicedata},{upsert:true},callbackfn);
       }
-      debug_historytrack(`stop dbh_historytrack`);
+    );
+  });
+  async.parallel(asyncfnsz,(err,result)=>{
+      debug_historytrack(`stop dbh_historydevice`);
       callbackfn(err,result);
-    });
-  }
-  else{
-    console.error(`dbh_device err,bulk is null`);
-    callbackfn();
-  }
+  });
+  // const bulk = dbModel.collection.initializeUnorderedBulkOp();
+  // if(!!bulk){
+  //   _.map(datas,(devicedata)=>{
+  //     bulk.insert(devicedata);
+  //     // bulk.find({
+  //     //     GUID:devicedata.GUID,
+  //     //     DeviceId:devicedata.DeviceId,
+  //     //     GPSTime:devicedata.GPSTime,
+  //     //   })
+  //     //   .upsert()
+  //     //   .updateOne({
+  //     //     $set:devicedata
+  //     //   });
+  //   });
+  //   bulk.execute((err,result)=>{
+  //     if(!!err){
+  //       console.error(`dbh_historytrack err`);
+  //       console.error(err);
+  //       console.error(err.stack);
+  //     }
+  //     debug_historytrack(`stop dbh_historytrack`);
+  //     callbackfn(err,result);
+  //   });
+  // }
+  // else{
+  //   console.error(`dbh_device err,bulk is null`);
+  //   callbackfn();
+  // }
   // dbModel.insertMany(datas, (err, result)=>{
   //   callbackfn(err,result);
   // });
