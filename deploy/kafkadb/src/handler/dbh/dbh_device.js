56d1dc251f78ca8c59e9733af16978077cfbe3f3
wangxiaoqing <xiaoqingwang@126.com>
2018-03-03 09:19:39 +0800

    非分片使用shard

--- deploy/kafkadb/src/handler/dbh/dbh_device.js
+++ deploy/kafkadb/src/handler/dbh/dbh_device.js
@@ -6,45 +6,45 @@ const async = require('async');
 const dbh_device =(datas,callbackfn)=>{
   const dbModel = DBModels.DeviceModel;
   debug_device(`start dbh_device,datas:${datas.length}`);
-  const asyncfnsz = [];
-  _.map(datas,(devicedata)=>{
-    asyncfnsz.push(
-      (callbackfn)=>{
-        dbModel.findOneAndUpdate({
-            DeviceId:devicedata.DeviceId
-         },{$set:devicedata},{upsert:true},callbackfn);
-      }
-    );
-  });
-  async.parallel(asyncfnsz,(err,result)=>{
-      debug_device(`stop dbh_device`);
-      callbackfn(err,result);
-  });
-  // const bulk = dbModel.collection.initializeUnorderedBulkOp();
-  // if(!!bulk){
-  //   _.map(datas,(devicedata)=>{
-  //     bulk.find({
-  //         DeviceId:devicedata.DeviceId
-  //       })
-  //       .upsert()
-  //       .updateOne({
-  //         $set:devicedata
-  //       });
-  //   });
-  //   bulk.execute((err,result)=>{
-  //     if(!!err){
-  //       console.error(`dbh_device err`);
-  //       console.error(err);
-  //       console.error(err.stack);
+  // const asyncfnsz = [];
+  // _.map(datas,(devicedata)=>{
+  //   asyncfnsz.push(
+  //     (callbackfn)=>{
+  //       dbModel.findOneAndUpdate({
+  //           DeviceId:devicedata.DeviceId
+  //        },{$set:devicedata},{upsert:true},callbackfn);
   //     }
+  //   );
+  // });
+  // async.parallel(asyncfnsz,(err,result)=>{
   //     debug_device(`stop dbh_device`);
   //     callbackfn(err,result);
-  //   });
-  // }
-  // else{
-  //   console.error(`dbh_device err,bulk is null`);
-  //   callbackfn();
-  // }
+  // });
+  const bulk = dbModel.collection.initializeUnorderedBulkOp();
+  if(!!bulk){
+    _.map(datas,(devicedata)=>{
+      bulk.find({
+          DeviceId:devicedata.DeviceId
+        })
+        .upsert()
+        .updateOne({
+          $set:devicedata
+        });
+    });
+    bulk.execute((err,result)=>{
+      if(!!err){
+        console.error(`dbh_device err`);
+        console.error(err);
+        console.error(err.stack);
+      }
+      debug_device(`stop dbh_device`);
+      callbackfn(err,result);
+    });
+  }
+  else{
+    debug_device(`dbh_device err,bulk is null`);
+    callbackfn();
+  }
 };
 
 module.exports = dbh_device;
