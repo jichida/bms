bfb926a87971e7d70a9c5c4bda1acfafe6fd5ef6
wangxiaoqing <xiaoqingwang@126.com>
2018-03-17 01:03:07 +0800

    去掉部分debug

--- deploy/kafkadb/src/handler/getalarmtxt.js
+++ deploy/kafkadb/src/handler/getalarmtxt.js
@@ -33,14 +33,14 @@ const getalarmtxt = (alarminfo)=>{
 }
 
 const bridge_alarminfo = (alarminfo)=>{
-  debug(`alarminfo===>${JSON.stringify(alarminfo)}`);
+  // debug(`alarminfo===>${JSON.stringify(alarminfo)}`);
   let alarmtxt = '';
   let alarminfonew = {};
   alarminfonew[`key`] = alarminfo._id;
   alarminfonew[`车辆ID`] = alarminfo.DeviceId;
   alarminfonew[`报警时间`] = alarminfo.DataTime;
   alarminfonew[`报警等级`] = alarminfo.warninglevel;
-  debug(`alarminfonew?===>${JSON.stringify(alarminfonew)}`);
+  // debug(`alarminfonew?===>${JSON.stringify(alarminfonew)}`);
   let alarminfotmp = _.clone(alarminfo);
   let rest = _.omit(alarminfotmp,['_id','CurDay','DeviceId','__v','DataTime','warninglevel','Longitude','Latitude']);
   // ////console.log(`rest===>${JSON.stringify(rest)}`);
@@ -50,9 +50,9 @@ const bridge_alarminfo = (alarminfo)=>{
       alarmtxt += `${keytxt} ${v}次|`
     }
   });
-  debug(`alarmtxt===>${alarmtxt}`);
+  // debug(`alarmtxt===>${alarmtxt}`);
   alarminfonew[`报警信息`] = alarmtxt;
-  debug(`alarminfonew===>${JSON.stringify(alarminfonew)}`);
+  // debug(`alarminfonew===>${JSON.stringify(alarminfonew)}`);
   return alarminfonew;
 }
 
