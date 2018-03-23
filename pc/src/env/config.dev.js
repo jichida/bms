8eb565db37e23dc21aaf211dc91cd3a1c8f50b69
wangxiaoqing <xiaoqingwang@126.com>
2018-03-23 21:13:14 +0800

    version

--- pc/src/env/config.dev.js
+++ pc/src/env/config.dev.js
@@ -1,4 +1,4 @@
-let islocalhost = true;
+let islocalhost = false;
 const serverurl = islocalhost?'http://localhost:5011':'http://101.89.141.109:81';
 const serverurlrestful = islocalhost?`${serverurl}/api`:`${serverurl}/apisrv/api`;
 const wspath = islocalhost?'/socket.io':'/apisrv/socket.io';
@@ -9,7 +9,7 @@ let config = {
     serverurl:`${serverurl}`,
     wspath:`${wspath}`,
     requesttimeout:5000,
-    appversion:'1.3.3',
+    appversion:'1.3.4',
     sendlocationinterval:20000,
     softmode:'pc',
     wendu_max: 80,
