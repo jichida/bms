3ce8d9d8719e68084282cbd621fed2a0df3fabd3
wangxiaoqing <xiaoqingwang@126.com>
2018-04-06 13:56:30 +0800

    no message

--- app/src/env/config.dev.js
+++ app/src/env/config.dev.js
@@ -9,7 +9,7 @@ let config = {
     serverurl:`${serverurl}`,
     wspath:`${wspath}`,
     requesttimeout:5000,
-    appversion:'1.3.6',
+    appversion:'1.3.6(build040616)',
     sendlocationinterval:20000,
     softmode:'app',
     sysinnerheight : window.innerHeight,
