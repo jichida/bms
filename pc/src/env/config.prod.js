6845f8ba132ca92fea697158ae611c93aa93e53a
wangxiaoqing <xiaoqingwang@126.com>
2018-04-09 18:11:36 +0800

    config

--- pc/src/env/config.prod.js
+++ pc/src/env/config.prod.js
@@ -9,7 +9,7 @@ let config = {
     serverurl:`${serverurl}`,
     wspath:`${wspath}`,
     requesttimeout:5000,
-    appversion:'1.3.12(build040901)',
+    appversion:'1.4.1(build040923)',
     sendlocationinterval:20000,
     softmode:'pc',
     wendu_max: 80,
