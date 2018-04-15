e66986b1d764ace4cbd10d12800a8c215a6a3458
wangxiaoqing <xiaoqingwang@126.com>
2018-04-15 10:26:08 +0800

    报警推送程序

--- deploy/srvpushapp/src/kafka_pushalaramtopic_app.js
+++ deploy/srvpushapp/src/kafka_pushalaramtopic_app.js
@@ -7,12 +7,55 @@ const getuserpushdeviceid = require('./getuserpushdeviceid.js');
 const _ = require('lodash');
 const debug = require("debug")("alarmpush");
 
+const getwarningleveltext = (warninglevel)=>{
+  const warninglevelmap = {
+    '高':'三级',
+    '中':'二级',
+    '低':'一级'
+  };
+  if(!!warninglevelmap[warninglevel]){
+    return warninglevelmap[warninglevel];
+  }
+  return warninglevel;
+}
+
+// const subString =(str, len, hasDot)=>{
+// 	var newLength=0;
+// 	var newStr="";
+// 	var chineseRegex=/[^\x00-\xff]/g;
+// 	var singleChar='';
+// 	var strLength=str.replace(chineseRegex,'**').length;
+// 	for(var i=0;i < strLength;i++){
+// 	singleChar=str.charAt(i).toString();
+// 	if(singleChar.match(chineseRegex) != null){
+// 		newLength+=2;
+// 	}else{
+// 		newLength++;
+// 	}
+// 	if(newLength>len){
+// 		break;
+// 	}
+// 	newStr+=singleChar;
+// 	}
+//
+// 	if(hasDot && strLength>len){
+// 		newStr+='...';
+// 	}
+// 	return newStr;
+// }
+
 const kafka_pushalaramtopic_app = (devicedata,callbackfn)=>{
+  if(devicedata['warninglevel'] !== '高' && devicedata['warninglevel'] !== '中' && devicedata['warninglevel'] !== '低' ){
+    debug(`报警等级为空,这条记录是:${JSON.stringify(devicedata)}`);
+    callbackfn();
+    return;
+  }
   const DeviceId = devicedata.DeviceId;
   const recordnew = {
     '车辆ID':_.get(devicedata,'DeviceId'),
     '报警时间':_.get(devicedata,'LastRealtimeAlarm.DataTime'),
-    '报警信息':_.get(devicedata,'alarmtxtstat')
+    '报警信息':_.get(devicedata,'alarmtxtstat'),
+    '报警等级':getwarningleveltext(_.get(devicedata,'warninglevel'))
   };
 
   getuserpushdeviceid(DeviceId,(userlist)=>{
@@ -33,11 +76,13 @@ const kafka_pushalaramtopic_app = (devicedata,callbackfn)=>{
         DeviceId:DeviceId,
         messagetype:'msg',
       }
+      // const msgalarm = `车辆:${recordnew['车辆ID']}于${recordnew['报警时间']}触发${recordnew['报警等级']}报警,报警信息:${recordnew['报警信息']}`,
+      const msgalarm = `车辆:${recordnew['车辆ID']}于${recordnew['报警时间']}触发${recordnew['报警等级']}报警,点击查看报警信息`;
       const messagenotify = {
         msgpayload,
         touserid:userid,
-        messagetitle:`车辆:${recordnew['车辆ID']}于${recordnew['报警时间']}报警,报警信息:${recordnew['报警信息']}`,
-        messagecontent:`车辆:${recordnew['车辆ID']}于${recordnew['报警时间']}报警,报警信息:${recordnew['报警信息']}`
+        messagetitle:msgalarm,
+        messagecontent:msgalarm
       };
       winston.getlog().info(`开始推送消息:${JSON.stringify(messagenotify)}`);
       // debug(`发送给用户${userid}=>${JSON.stringify(messagenotify)}`);
