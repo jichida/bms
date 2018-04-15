4d5d083703daf7a41e0f1bcd7d6534834ebcd036
wangxiaoqing <xiaoqingwang@126.com>
2018-04-15 20:13:51 +0800

    no message

--- pc/src/env/config.prod.js
+++ pc/src/env/config.prod.js
@@ -1,5 +1,5 @@
 let islocalhost = false;
-const serverurl = islocalhost?'http://localhost:5011':'http://121.204.130.141:81';
+const serverurl = islocalhost?'http://localhost:5011':'http://121.204.128.149:81';
 const serverurlrestful = islocalhost?`${serverurl}/api`:`${serverurl}/apisrv/api`;
 const wspath = islocalhost?'/socket.io':'/apisrv/socket.io';
 // const organizationid = '599af5dc5f943819f10509e6';
