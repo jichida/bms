1898fa4a5d12342f6591b2a76df6ca5a70f0699c
wangxiaoqing <xiaoqingwang@126.com>
2018-04-25 15:28:35 +0800

    no message

--- deploy/srvintervalexport/src/config.js
+++ deploy/srvintervalexport/src/config.js
@@ -8,7 +8,7 @@ const config =  {
   DeviceId:process.env.DeviceId,
   exportFlag:process.env.exportFlag || 'all',
   exportdir:process.env.exportdir ||'../../dist/exportdir',
-  version:'1.0.0',
+  version:'1.0.1',
   mapdict:{},
 };
 
