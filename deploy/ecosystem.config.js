fcb122ab4675adc1aa366133af176c5ba6b1c83c
wangxiaoqing <xiaoqingwang@126.com>
2018-05-03 03:00:59 +0800

    no message

--- deploy/ecosystem.config.js
+++ deploy/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
       {
         name: "srvexport",
-        script: "./index.js",
+        script: "./srvintervalexport/index.js",
         watch: true,
         env: {
           "NODE_ENV": "production",
