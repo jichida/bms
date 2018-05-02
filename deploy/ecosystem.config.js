59bc8cb2c6f6865a534b8a7b91a35891950c7202
wangxiaoqing <xiaoqingwang@126.com>
2018-05-03 04:04:07 +0800

    no message

--- deploy/ecosystem.config.js
+++ deploy/ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
           "MONGO_URL":"mongodb://192.168.2.17:27007,192.168.2.18:27007/bmscatl",
           "mongos":"true",
           "logdir":"/root/bms/deploy/dist/log",
-          "exportdir":"exportdir=/root/bms/deploy/dist/exportdir"
+          "exportdir":"/root/bms/deploy/dist/exportdir"
         }
       }
   ]
