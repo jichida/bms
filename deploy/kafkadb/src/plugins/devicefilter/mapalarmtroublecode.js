b2f176562fc0478b0feb0656d524d71f662db4a2
wangxiaoqing <xiaoqingwang@126.com>
2018-05-02 20:14:57 +0800

    改为大写

--- deploy/kafkadb/src/plugins/devicefilter/mapalarmtroublecode.js
+++ deploy/kafkadb/src/plugins/devicefilter/mapalarmtroublecode.js
@@ -4,14 +4,14 @@ const getFieldname = (cantype,errorcode)=>{
 
 	if(cantype === 0 || cantype === 1){
 		//0/1-->51
-		return `AL_Trouble_Code_51_${errorcode}`;
+		return `AL_TROUBLE_CODE_51_${errorcode}`;
 	}
 	else if(cantype === 1){
 		//2-->6.0
-		return `AL_Trouble_Code_60_${errorcode}`;
+		return `AL_TROUBLE_CODE_60_${errorcode}`;
 	}
 	else{
-		return `AL_Trouble_Code_${errorcode}`;
+		return `AL_TROUBLE_CODE_${errorcode}`;
 	}
 }
 
