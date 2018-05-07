6f001262e74492397f91626f02295f49adb29b3b
wangxiaoqing <xiaoqingwang@126.com>
2018-05-07 13:51:11 +0800

    ====

--- admin/src/env/config.prod.js
+++ admin/src/env/config.prod.js
@@ -1,11 +1,11 @@
 let islocalhost = false;
-let serverurl = islocalhost?'http://localhost:5011':'http://bmstest.i2u.top:81/apisrv';
+let serverurl = islocalhost?'http://localhost:5011':'http://bmscatl.i2u.top:81/apisrv';
 const organizationid = '599af5dc5f943819f10509e6';
 export default {
     restserverurl:`${serverurl}/adminapi/v1/${organizationid}`,
     adminauthserverurl:`${serverurl}/adminauth/v1/${organizationid}`,
     admincustomapi:`${serverurl}/admincustomapi/v1/${organizationid}`,
     serverurl:`${serverurl}`,
-    appversion:'1.3.6',
+    appversion:'1.3.7(build0507)',
     listperpage:100
 };
