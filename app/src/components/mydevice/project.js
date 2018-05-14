c0d66cbfd22b7fc7692ebb56e05e0d6d173f8326
wangxiaoqing <xiaoqingwang@126.com>
2018-05-14 13:19:05 +0800

    定位逻辑 [App]从车辆信息和详情页没法定位车辆及显示tip框 样式需要美化

--- app/src/components/mydevice/project.js
+++ app/src/components/mydevice/project.js
@@ -8,7 +8,7 @@ import map from 'lodash.map';
 // import Moresetting from "../../img/17.png";
 // import Footer from "../index/footer.js";
 
-import {ui_viewdevicedetail} from '../../actions';
+import {ui_viewdevicedetail,ui_alarm_selcurdevice} from '../../actions';
 // import Searchimg from "../../img/22.png";
 import SelectDevice from '../mydevice/selectdevice.js';
 
@@ -36,6 +36,9 @@ class Page extends React.Component {
     rowClick = (id)=>{
       this.props.dispatch(ui_viewdevicedetail({DeviceId:id}));
         // this.props.history.push(`/deviceinfo/${id}`);
+    }
+    onClickPos = (id)=>{
+      this.props.dispatch(ui_alarm_selcurdevice(id));
     }
     render() {
         let deviceidlist = [];
@@ -102,7 +105,12 @@ class Page extends React.Component {
                         {  !!groups[groupid].deviceids && groups[groupid].deviceids.length >0 &&
                             map(groups[groupid].deviceids,(data)=>{
                                 return <div key={data.DeviceId}
-                                   onClick={()=>{this.rowClick(data.DeviceId)}} style={{textAlign: "center",lineHeight : "46px", borderBottom:"1px solid #EEE",fontSize: "16px"}}>{data.DeviceId}</div>
+                                    style={{textAlign: "center",lineHeight : "46px", borderBottom:"1px solid #EEE",fontSize: "16px"}}>
+                                   <span onClick={()=>{this.rowClick(data.DeviceId)}}>{data.DeviceId}</span>
+                                   <span onClick={()=>
+                                     {this.onClickPos(data.DeviceId)}
+                                   }>定位</span>
+                                 </div>
                             })
                         }
                     </div>
