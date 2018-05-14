d0eeab266a73d8fb04ef9c7fa2a2af6304f88594
wangxiaoqing <xiaoqingwang@126.com>
2018-05-14 11:02:24 +0800

    [PC]详情页面左边图表SOC折线图采集点改成分钟，横坐标的时间还是取10小时的数据不变 #772 放到单点上的数据显示没有了，需要加上：时间，value

--- pc/src/components/device/chart_rechart.js
+++ pc/src/components/device/chart_rechart.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import {LineChart, Line, XAxis, YAxis} from 'recharts';
-
+import {LineChart, Line, XAxis, YAxis,Tooltip} from 'recharts';
+//
+// const CustomTooltip = (props)=>{
+//
+//   const { active } = props;
+//
+//   if (active) {
+//     const { payload, label } = props;
+//     console.log(`payload:${JSON.stringify(payload)},label:${label}`)
+//     return (
+//       <div className="custom-tooltip">
+//         <p className="label">{`时间:${label}`}</p>
+//         <p className="label">{`值:${payload[0].value}`}</p>
+//       </div>
+//     );
+//   }
+//
+//   return null;
+// }
 
 const Rechart = (props)=>{
-  const {chartdata} = props;
+  const {chartdata,unit} = props;
   return (
     <LineChart width={window.innerWidth} height={300} data={chartdata}
         margin={{top: 20, right: 20, left: 20, bottom: 20}}>
      <XAxis dataKey="time"/>
      <YAxis/>
-     <Line type="monotone" dataKey="value"  stroke="#82ca9d" isAnimationActive={false}/>
+     {/* <Tooltip  content={<CustomTooltip/>}/> formatter={(value, name, props) => {
+       name='值';
+       console.log(`name:${name},value:${value}`)
+       return `${value}`;
+     }}*/}
+     <Tooltip
+       formatter={(value, name, props) => {
+         return `${value}${unit}`;
+       }}
+       labelFormatter={
+       (label)=>{
+         console.log(`labelFormatter--->props:${JSON.stringify(props)}`)
+         return `时间 : ${label}`;
+       }
+     } />
+
+     <Line type="monotone" dataKey="值"  stroke="#82ca9d" isAnimationActive={false}/>
     </LineChart>
   );
 }
