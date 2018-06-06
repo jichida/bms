4d4199e8ca08f4d6d286e92f56cc2b2eaa2c4b52
wangxiaoqing <xiaoqingwang@126.com>
2018-06-06 18:05:12 +0800

    分组程序更新

--- deploy/srvdevicegroupcron/src/lib/startcron_updatedevicegroup.js
+++ deploy/srvdevicegroupcron/src/lib/startcron_updatedevicegroup.js
@@ -5,25 +5,68 @@ const mongoose = require('mongoose');
 const moment = require('moment');
 const debug = require('debug')('srvdevicegroupcron:startcron');
 
-const startcron_updatedevicegroup = (retmapgroupidtodevice,callbackfn)=>{
-  const dbDeviceGroupModel = DBModels.DeviceGroupModel;
-  let fnsz = [];
-  const updatetime = moment().format('YYYY-MM-DD HH:mm:ss');
-  _.map(retmapgroupidtodevice,(deviceidslist,groupid)=>{
-    fnsz.push((callback)=>{
-      dbDeviceGroupModel.findOneAndUpdate({_id:groupid}, {$set:{
-          deviceids:deviceidslist,
-          updatetime
-        }},{new: true,upsert:true},(err,result)=>{
-          debug(`result--->${groupid}-->${deviceidslist.length}`);
-          callback(null,true);
-      });
-    });
+const getdevicecites = (callbackfn)=>{
+  const dbModel =  DBModels.DeviceCityModel;
+  dbModel.aggregate([
+    {
+  		$group: {
+  			_id:'$citycode',
+  			deviceids: { $addToSet: "$deviceid" },
+        province: { $first: "$province" },
+        city: { $first: "$city" },
+  		}
+  	}
+  ]).exec((err, list)=> {
+    callbackfn(err,list);
   });
+}
 
-  async.parallel(fnsz,(err,result)=>{
-    callbackfn(err,result);
-  });
+const startcron_updatedevicegroup = (callback)=>{
+  const dbDeviceGroupModel = DBModels.DeviceGroupModel;
+  const updatetime = moment().format('YYYY-MM-DD HH:mm:ss');
+  getdevicecites((err,citylist)=>{
+    if(!err && !!citylist){
+      _.map(citylist,(v)=>{
+          // console.log(v);
+          let ObjSet = {};
+          const citycode = _.get(v,'_id','');
+          const deviceidslist = _.get(v,'deviceids',[]);
+          const province = _.get(v,'province','');
+          const city = _.get(v,'city','');
+          let name = `${province}`;
+          if(city.length > 0){
+            name = `${name}${city}`;
+          }
+          ObjSet[`$setOnInsert`] = {name};
+          ObjSet[`$set`] ={
+              deviceids:deviceidslist,
+              updatetime
+            };
+          dbDeviceGroupModel.findOneAndUpdate({citycode},ObjSet,{new: true,upsert:true},(err,result)=>{
+              debug(`result-->${citycode}--->${result._id}-->${deviceidslist.length}`);
+              callback(null,true);
+          });
+        });//map
+    }
+  })
+  // const dbDeviceGroupModel = DBModels.DeviceGroupModel;
+  // let fnsz = [];
+  // const updatetime = moment().format('YYYY-MM-DD HH:mm:ss');
+  // _.map(retmapgroupidtodevice,(deviceidslist,groupid)=>{
+  //   fnsz.push((callback)=>{
+  //     dbDeviceGroupModel.findOneAndUpdate({_id:groupid}, {$set:{
+  //         deviceids:deviceidslist,
+  //         updatetime
+  //       }},{new: true,upsert:true},(err,result)=>{
+  //         debug(`result--->${groupid}-->${deviceidslist.length}`);
+  //         callback(null,true);
+  //     });
+  //   });
+  // });
+  //
+  // async.parallel(fnsz,(err,result)=>{
+  //   callbackfn(err,result);
+  // });
 }
 
 module.exports = startcron_updatedevicegroup;
