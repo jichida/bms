2f248177f0d6f96740439e04f8cb391b371b55aa
wangxiaoqing <xiaoqingwang@126.com>
2018-10-16 20:56:06 +0800

    remove redis solution

--- deploy/srvintervalexport/src/srvsys.js
+++ deploy/srvintervalexport/src/srvsys.js
@@ -1,7 +1,7 @@
 const export_position = require('./lib/export_position');
 const export_alarm = require('./lib/export_alarm');
-const export_history = require('./lib/export_history_redis');
-const redisdata = require('./redis/getdata.js');
+const export_history = require('./lib/export_history_shell');
+// const redisdata = require('./redis/getdata.js');
 const export_device_ext = require('./lib/export_device_ext');
 const _ = require('lodash');
 const debug = require('debug')('srvinterval:test');
@@ -54,7 +54,7 @@ const cron_0 = (callbackfn)=>{
     const curday = moments.format('YYYYMMDD');
     debug(`start export file:${curday}`);
     //使用getdata_lpop导出数据，这样可以清空redis中数据缓存
-    export_history(curday,redisdata.getDevicelist,redisdata.getdata_lpop,(exportdir)=>{
+    export_history(curday,(exportdir)=>{
       curtime = moment().format('YYYY-MM-DD HH:mm:ss');
       winston.getlog().info(`导出历史记录完毕:${exportdir}-->${curtime}`);
       // {"_id":"5aab1f84b8495bf6d0bc893a","DeviceId":"1602010031","LastRealtimeAlarm":{"DataTime":"2018-03-27 05:37:40"},"LastHistoryTrack":{"GPSTime":"2018-03-27 05:39:09","Longitude":121.59474,"Latitude":31.266263},"alarmtxtstat":"F[104] 252次|加热继电器故障 614次|F[155] 282次|F[118] 16次|F[140] 33次|F[166] 27次|F[164] 1次|F[167] 3次|","Provice":"上海市","City":"未知","Area":"浦东新区"}
@@ -177,19 +177,19 @@ const start_cron18 = ()=>{
   });
 }
 
-
-const start_croneveryhours = (callbackfn)=>{
-  const moments = moment();
-  const curday = moments.format('YYYYMMDD');
-  debug(`开始导出当天数据:${curday}`);
-  export_history(curday,redisdata.getDevicelist,redisdata.getdata_lpop,(exportdir)=>{
-    callbackfn(exportdir);
-  });
-}
+//
+// const start_croneveryhours = (callbackfn)=>{
+//   const moments = moment();
+//   const curday = moments.format('YYYYMMDD');
+//   debug(`开始导出当天数据:${curday}`);
+//   export_history(curday,redisdata.getDevicelist,redisdata.getdata_lpop,(exportdir)=>{
+//     callbackfn(exportdir);
+//   });
+// }
 
 
 
 
 exports.start_cron0 = start_cron0;
 exports.start_cron18 = start_cron18;
-exports.start_croneveryhours = start_croneveryhours;
+// exports.start_croneveryhours = start_croneveryhours;
