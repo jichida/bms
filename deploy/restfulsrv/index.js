9f8d66e6da84dc9498ca09467b487d7e1add1ea2
wangxiaoqing <xiaoqingwang@126.com>
2019-01-18 12:49:51 +0800

    ====

--- deploy/restfulsrv/index.js
+++ deploy/restfulsrv/index.js
@@ -78,6 +78,7 @@ debug(`issmsdebug:${config.issmsdebug}`);
 srvsystem.job();
 srvwebsocket.startsrv(srvhttp.startsrv());
 
-// process.on('uncaughtException', (err)=> {
-//     console.log(err);
-// });
+process.on('uncaughtException', (err)=> {
+  debug(err);
+  winston.getlog().info(`发生异常了...${JSON.stringify(err)}`);
+});
