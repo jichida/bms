d648d5b9df716e6bfea543fe049e4eaa6521f752
wangxiaoqing <xiaoqingwang@126.com>
2019-02-21 10:29:43 +0800

    修正timekey问题

--- deploy/restfulsrv/src/handler/common/historydevice.js
+++ deploy/restfulsrv/src/handler/common/historydevice.js
@@ -173,7 +173,8 @@ const deviceinfoquerychart =  (actiondata,ctx,callback)=>{
 const gettimekey =(timestart,timeend)=> {
     const timekeysz = [];
     const momentstart = moment(timestart);
-    const momentend = moment(timeend);
+    const timeendday = moment(timeend).format('YYYY-MM-DD 23:59:59');
+    const momentend = moment(timeendday);
     let momenti;
     for(momenti =momentstart ;momenti <= momentend;  ){
       const timekey = momenti.format('YYMMDD');
